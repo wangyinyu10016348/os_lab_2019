4be2c216bc099ed7c97ff359ce0404107db3ec7b
wangyinyu10016348 <yinyuwang9982@gmail.com>
2020-03-27 08:51:28 +0000

    add

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,12 @@
 #include "revert_string.h"
-
+#include <string.h>
 void RevertString(char *str)
 {
-	// your code here
+	for (int i = 0, l = strlen(str); i < l / 2; i++) 
+    {
+		char c = str[i];
+		str[i] = str[l - i - 1];
+		str[l - i - 1] = c;
+    }
 }
 
