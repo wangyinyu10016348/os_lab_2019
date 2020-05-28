7dcb9d51ec5d7925b833d2a75b3f0b743b088895
wangyinyu10016348 <yinyuwang9982@gmail.com>
2020-05-28 18:29:21 +0000

    add

---
+++ lab5/src/update.sh
@@ -0,0 +1,7 @@
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install nano
+sudo apt -y install clang-format
+sudo apt -y install iputils-ping
+sudo apt -y install gcc
+sudo apt -y install libcunit1 libcunit1-doc libcunit1-dev
